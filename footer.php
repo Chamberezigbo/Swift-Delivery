65a828364b6fb4f31abad77f658edf2d5da8cff2
chamberezigbo <chamberezigbo@gmail.com>
2022-05-06 11:47:16 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,76 @@
+<footer>
+     <section>
+          <div class="container pt-4">
+               <div class="row">
+                    <div class="col-lg-8">
+                         <h6><strong>Deutsche Post DHL Group</strong></h6>
+                         <div class="row">
+                              <div class="col-lg-3 col-6">
+                                   <p class="fw-1 card-text-hover">Fraud Awareness</p>
+                              </div>
+                              <div class="col-lg-2 col-6 ">
+                                   <P class="fw-1 card-text-hover"> Legal Notice </P>
+                              </div>
+                              <div class="col-lg-2 col-6">
+                                   <P class="fw-1 card-text-hover"> Terms of Use </P>
+                              </div>
+                              <div class="col-lg-2 col-6">
+                                   <P class="fw-1 card-text-hover"> Privacy Notice </P>
+                              </div>
+                              <div class="col-lg-2 col-6">
+                                   <P class="fw-1 card-text-hover"> Dispute Resolution</P>
+                              </div>
+                              <div class="col-lg-3 col-6">
+                                   <P class="fw-1 card-text-hover"> Additional Information</P>
+                              </div>
+                              <div class="col-lg-2 col-6">
+                                   <P class="fw-1 card-text-hover"> Cooking Setting </P>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                         <h6><strong>Follow Us</strong></h6>
+                         <div>
+                              <i class="fa-brands fa-facebook fa-2x"></i>
+                              <i class="fa-brands fa-instagram fa-2x"></i>
+                              <i class="fa-brands fa-youtube fa-2x"></i>
+                              <i class="fa-brands fa-linkedin fa-2x"></i>
+                         </div>
+                    </div>
+               </div>
+          </div>
+          <div class="text-center mt-3">
+               <h6>2022 &#169- all alright reserved</h6>
+          </div>
+     </section>
+</footer>
+
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script>
+     setInterval(function() {
+          let alertDev = document.getElementById('alertActivation').classList.add("d-none")
+     }, 5500);
+</script>
+
+<script type="text/javascript">
+     function googleTranslateElementInit() {
+          new google.translate.TranslateElement({
+               pageLanguage: 'en',
+               layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+          }, 'google_translate_element');
+     }
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="res/css/main.style.css">
+     <link rel="stylesheet" href="res/css/global.style.css">
+
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <link rel="stylesheet" href="res/fontawesome-free-6.0.0-web/css/all.css">
+     <link rel="shortcut icon" href="favicon.ico" />
+
+     <title>Global ....</title>
+</head>
+
+<body>
+     <div class="container-fluid ">
+          <nav class="navbar navbar-expand-lg navbar-light fixed-top first-nav">
+               <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                         <img src="res/image/logistic-logo.png" alt="" width="70" height="35">
+                    </a>
+                    <a class="navbar-brand mt-2" href="#">
+                         <h5>Swift Delivery Cargo</h5>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                         <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                         <ul class="navbar-nav ms-auto">
+                              <li class="nav-item">
+                                   <a class="nav-link active" href="#home">Home</a>
+                              </li>
+                              <li class="nav-item">
+                                   <a class="nav-link" href="#scrollspyHeading1">Contact Us</a>
+                              </li>
+                              <li class="nav-item">
+                                   <a class="nav-link" href="#"><i class="fa-solid fa-circle-exclamation"></i> Alert(2)</a>
+                              </li>
+                              <li class="nav-item">
+                                   <a class="nav-link" href="#"><i class="fa-solid fa-globe"></i>USA</a>
+                              </li>
+                              <li class="nav-item">
+                                   <div class="mt-2" id="google_translate_element"></div>
+                              </li>
+                         </ul>
+                    </div>
+               </div>
+          </nav>
+
+     </div>
